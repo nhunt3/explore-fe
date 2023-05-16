358da31bbb5c609c47dedd5d4a30e2b987682453
Nick Hunt <Nicholas.Hunt@coxautoinc.com>
2023-05-16 18:03:50 -0400

    replacing webpack and react-scripts with vite

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//     <React.StrictMode>
+//         <App />
+//     </React.StrictMode>
+// );
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
