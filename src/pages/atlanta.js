6c5e06d93178a556c76d53c5ed6c713466a95b3e
Nick Hunt <Nicholas.Hunt@coxautoinc.com>
2023-05-15 23:47:24 -0400

    satified linter

--- src/pages/atlanta.js
+++ src/pages/atlanta.js
@@ -10,19 +10,19 @@ export const Atlanta = () => {
             </div>
             <div className='section-container'>
                 <div>
-                    <img className='image-small' src={`${config.s3baseUrl}hang out/Eastside_Trail.jpg`} />
+                    <img className='image-small' alt='beltline' src={`${config.s3baseUrl}hang out/Eastside_Trail.jpg`} />
                     <div className='section-header'>
                         Hang Out
                     </div>
                 </div>
                 <div>
-                    <img className='image-small' src={`${config.s3baseUrl}work remotely/cold brew bar - inside looking outside.jpg`} />
+                    <img className='image-small' alt='cold brew bar' src={`${config.s3baseUrl}work remotely/cold brew bar - inside looking outside.jpg`} />
                     <div className='section-header'>
                         Work Remotely
                     </div>
                 </div>
                 <div>
-                    <img className='image-small' src={`${config.s3baseUrl}nightlife/flight club darts.jpg`} />
+                    <img className='image-small' alt='flight club darts' src={`${config.s3baseUrl}nightlife/flight club darts.jpg`} />
                     <div className='section-header'>
                         Nightlife
                     </div>
