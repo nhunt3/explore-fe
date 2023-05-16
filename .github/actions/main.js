04f65714856486af2534f35af3dee3349c1d8a7f
Nick Hunt <Nicholas.Hunt@coxautoinc.com>
2023-05-16 18:49:01 -0400

    seting up deploy to s3 gha

---
+++ .github/actions/main.js
@@ -0,0 +1,19 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const run = () => {
+    // Get input files
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // upload files to s3
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    // output url
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
+};
+
+run();
